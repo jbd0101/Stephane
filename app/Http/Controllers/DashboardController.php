93291a464604d8dd80d6d9641e270b643427a50c
jean-christophe bauduin <maildejc@hotmail.be>
2017-07-02 13:23:23 +0200

    delete old informations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,6 +10,8 @@ use Ixudra\Curl\Facades\Curl;
 class DashboardController extends Controller
 {
     function index(){
+        Data::where('created_at', '<=', Carbon::now()->subDay(6))->delete();
+
     	$data = Data::where('created_at', '>=', Carbon::now()->subDay(2))->get()->toArray();
     	$derniere_pluie = Data::where('pluie',true)->where("arrosage",false)->limit(1)->get()->toArray();
     	return view("dashboard",["data"=>$data,"derniere_pluie" => $derniere_pluie]);
