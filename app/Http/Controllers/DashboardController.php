07f17229cc63da64c4214bfca2dbf61beca21244
jean-christophe bauduin <maildejc@hotmail.be>
2017-08-04 16:42:54 +0200

    data 1 day

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,7 +12,7 @@ class DashboardController extends Controller
     function index(){
         Data::where('created_at', '<=', Carbon::now()->subDay(6))->delete();
 
-    	$data = Data::where('created_at', '>=', Carbon::now()->subDay(2))->get()->toArray();
+    	$data = Data::where('created_at', '>=', Carbon::now()->subDay(1))->get()->toArray();
     	$derniere_pluie = Data::where('pluie',true)->where("arrosage",false)->limit(1)->get()->toArray();
     	return view("dashboard",["data"=>$data,"derniere_pluie" => $derniere_pluie]);
     }
